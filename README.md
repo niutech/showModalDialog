e21fa0b773589b85eea9b89fb2432c16e91d22d8
niutech <jerzyglowacki@gmail.com>
2014-08-05 03:32:49 +0200

    New version using ES6 Generators

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
 ShowModalDialog Polyfill
 ========================
 
-This is a `window.showModalDialog()` shim using a modal `<dialog>` element. It was tested in the latest Google Chrome. Just include the following HTML code before using that function: `<script src="showModalDialog.js"></script>`.
+This is a `window.showModalDialog()` shim using a modal HTML5 `<dialog>` element and ECMAScript 6 Generators. It was tested in the latest Google Chrome with the *Enable Experimental JavaScript* flag enabled in [chrome://flags](chrome://flags/#enable-javascript-harmony). Just include the following HTML code before using `showModalDialog` function: `<script src="showModalDialog.js"></script>`.
 
 Passing both `window.dialogArguments` and `window.returnValue` is supported, provided that the dialog document is on the same server as the host document.
+
+ShowModalDialog Polyfill is using Promises, Generators, the `yield` keyword and the [`spawn` function](https://gist.github.com/jakearchibald/31b89cba627924972ad6) by Jake Archibald. An old version using a callback function is still available.
 
 Syntax
 ------
 
-`window.showModalDialog( url [, arguments, options, callback] )`
+```
+spawn(function*() {
+
+    //statements before showing a modal dialog
+
+    var returnValue = yield window.showModalDialog( url [, arguments, options] );
+
+    //statements after closing a modal dialog
+
+});
+```
 
 Where:
 
  - *url* - a string that specifies the URL of the document to load and display;
  - *arguments* - a variant that specifies the arguments to use when displaying the document;
  - *options* - a string that specifies the dialog box style, using CSS or the following semicolon-delimited attributes: *dialogHeight:???px;dialogLeft:???px;dialogTop:???px;dialogWidth:???px*;
-  - *callback* - a function that is invoked after closing the dialog box, with a single parameter *returnValue*.
 
 Demo
 ----
 
-[Here is a live demo](http://niutech.github.com/showModalDialog/demo.html). Works in Google Chrome 37+.
+[Here is a live demo](http://niutech.github.com/showModalDialog/demo.html). Works in Google Chrome 37+ with the *Enable Experimental JavaScript* flag.
 
 License
 -------
--- showModalDialog.js
+++ showModalDialog.js
@@ -1,23 +1,42 @@
 (function() {
-    window.showModalDialog = window.showModalDialog || function(url/*, arg, opt, cb*/) {
-	var arg = arguments[1] || null; //dialog arguments
-	var opt = arguments[2] || "dialogWidth:300px;dialogHeight:200px"; //options: dialogTop;dialogLeft;dialogWidth;dialogHeight or CSS
-	var cb = arguments[3] || function() {}; //callback on dialog close
-	var dialog = document.body.appendChild(document.createElement('dialog'));
-	opt = opt.replace(/dialog/gi, '');
-	dialog.setAttribute('style', opt);
-	dialog.innerHTML = '<a href="#" id="dialog-close" style="position: absolute; top: 0; right: 4px; font-size: 20px; color: #000; text-decoration: none; outline: none;">&times;</a><iframe id="dialog-body" src="' + url + '" style="border: 0; width: 100%; height: 100%;"></iframe>';
-	document.getElementById('dialog-body').contentWindow.dialogArguments = arg;
-	document.getElementById('dialog-close').addEventListener('click', function(e) {
-		e.preventDefault();
-		dialog.close();
-	});
-	dialog.addEventListener('close', function() {
-		dialog.returnValue = document.getElementById('dialog-body').contentWindow.returnValue;
-		document.body.removeChild(dialog);
-		cb(dialog.returnValue);
-	});
-	dialog.showModal();
-	return dialog.returnValue;
+    window.spawn = window.spawn || function(gen) {
+        function continuer(verb, arg) {
+            var result;
+            try {
+                result = generator[verb](arg);
+            } catch (err) {
+                return Promise.reject(err);
+            }
+            if (result.done) {
+                return result.value;
+            } else {
+                return Promise.resolve(result.value).then(onFulfilled, onRejected);
+            }
+        }
+        var generator = gen();
+        var onFulfilled = continuer.bind(continuer, "next");
+        var onRejected = continuer.bind(continuer, "throw");
+        return onFulfilled();
+    }
+    window.showModalDialog = window.showModalDialog || function(url/*, arg, opt*/) {
+        var arg = arguments[1] || null; //dialog arguments
+        var opt = arguments[2] || "dialogWidth:300px;dialogHeight:200px"; //options: dialogTop;dialogLeft;dialogWidth;dialogHeight or CSS styles
+        return new Promise(function(resolve, reject) {
+            var dialog = document.body.appendChild(document.createElement('dialog'));
+            opt = opt.replace(/dialog/gi, '');
+            dialog.setAttribute('style', opt);
+            dialog.innerHTML = '<a href="#" id="dialog-close" style="position: absolute; top: 0; right: 4px; font-size: 20px; color: #000; text-decoration: none; outline: none;">&times;</a><iframe id="dialog-body" src="' + url + '" style="border: 0; width: 100%; height: 100%;"></iframe>';
+            document.getElementById('dialog-body').contentWindow.dialogArguments = arg;
+            document.getElementById('dialog-close').addEventListener('click', function(e) {
+                e.preventDefault();
+                dialog.close();
+            });
+            dialog.addEventListener('close', function() {
+                var returnValue = document.getElementById('dialog-body').contentWindow.returnValue;
+                document.body.removeChild(dialog);
+                resolve(returnValue);
+            });
+            dialog.showModal();
+        });
     };
 })();
